424204a2bbd3d64e28f60d6ac2e682b7c07cd4bc
Hasan Hüseyin <hasanhuseyinbagg@gmail.com>
2021-12-25 19:38:11 +0300

    Ağaç Modeli Eklendi
    -Ağaç modeli Eklendi

--- WheellessDriving/Assets/Assets/Scripts/CameraFollow.cs
+++ WheellessDriving/Assets/Assets/Scripts/CameraFollow.cs
@@ -5,12 +5,13 @@ using UnityEngine;
 public class CameraFollow : MonoBehaviour
 {
     Transform Araba;
+    [SerializeField] float Y_ekseni, X_ekseni;
     void Start()
     {
         Araba = GameObject.FindGameObjectWithTag("Araba").transform;
     }
     void Update()
     {
-        transform.position = new Vector3(0, Araba.position.y + 15, Araba.position.z - 12.5f);
+        transform.position = new Vector3(0, Araba.position.y + Y_ekseni, Araba.position.z - X_ekseni);
     }
 }
